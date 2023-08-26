cfa72c0d371f4e17904179ad797d1f6aca8cad6c
florencio <florencio@razziefox.com>
2023-08-26 12:12:31 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// bunfetch-cs developed by florencio
+// licensed under the MIT license
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace bunfetch
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            var os = Environment.OSVersion;
+
+            Console.WriteLine(" (\\_/)      kernel: {0} {1}\n ( . .)     architecture: {2} \nC(\")(\")", os.Platform.ToString(), os.Version.ToString(), RuntimeInformation.OSArchitecture.ToString());
+
+        }
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+## bunfetch-cs
+bunfetch rewritten entirely in C# displaying system information with a cute bunny.
+
+## dependencies
+* .NET 7
+
+## building
+* run ```dotnet publish -c Release```
